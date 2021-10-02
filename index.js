9bab11ce2c30f2df7e0416e5225a89d31abf42fc
Michael Caterisano <mcaterisano@gmail.com>
2021-10-02 19:21:32 -0400

    finish server

---
+++ index.js
@@ -0,0 +1,18 @@
+// require('dotenv').config();
+// const Twitter = require('twitter-v2');
+
+// const client = new Twitter({
+//   consumer_key: process.env.TWITTER_API_KEY,
+//   consumer_secret: process.env.TWITTER_API_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// });
+
+// const getTweets = async () => {
+//   const { data } = await client.get('users/by/username/mttsrgnt');
+//   return { data };
+// };
+
+// getTweets();
+
+// //mttsrgnt id: 380051971
---
+++ server.js
@@ -0,0 +1,58 @@
+require('dotenv').config()
+const fastify = require('fastify')({ logger: true })
+const needle = require('needle')
+
+// this is the ID for @mttsrgnt
+const userId = 380051971
+const url = `https://api.twitter.com/2/users/${userId}`
+const bearerToken = process.env.TWITTER_API_BEARER_TOKEN
+
+const getFollowersFollowing = async () => {
+    let params = {
+        'user.fields': 'public_metrics',
+    }
+
+    const options = {
+        headers: {
+            'User-Agent': 'v2FollowersJS',
+            authorization: `Bearer ${bearerToken}`,
+        },
+    }
+
+    try {
+        const resp = await needle('get', url, params, options)
+
+        if (resp.statusCode != 200) {
+            console.log(
+                `${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`
+            )
+            return
+        }
+
+        const { data } = resp.body
+        const { followers_count: followers, following_count: following } =
+            data.public_metrics
+        return { followers, following }
+    } catch (err) {
+        throw new Error(`Request failed: ${err}`)
+    }
+}
+
+fastify.get('/followers', async (request, reply) => {
+    reply.header('Access-Control-Allow-Origin', '*')
+    reply.header('Access-Control-Allow-Methods', 'GET')
+
+    const { followers, following } = await getFollowersFollowing()
+    return { followers, following }
+})
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(3000)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
