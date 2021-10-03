f5434acda691c4306258b956cdf6e6e4b414c228
Michael Caterisano <mcaterisano@gmail.com>
2021-10-02 20:06:16 -0400

    try 0:0:0:0 host

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ fastify.get('/followers', async (request, reply) => {
 // Run the server!
 const start = async () => {
     try {
-        await fastify.listen(process.env.PORT || 3000)
+        await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
